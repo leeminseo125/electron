a1b7597a86a050754c1d85eb71156380675fc707
kty2001 <tyyoujungzz@naver.com>
2024-11-18 18:43:49 +0900

    asdf

--- frontend/static/renderer.js
+++ frontend/static/renderer.js
@@ -22,7 +22,7 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
       
       console.log('Sending file:', file.name, file.type);
 
-      const response = await fetch('http://0.0.0.0:8000/classify', {
+      const response = await fetch('http://54.180.86.10:8000/classify', {
           method: 'POST',
           body: formData,
           headers: {
