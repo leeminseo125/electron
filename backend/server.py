9e4b82cf12e03abf76ef31f5d29987ca114950c9
kty2001 <tyyoujungzz@naver.com>
2024-11-19 14:20:51 +0900

    qwer

--- backend/server.py
+++ backend/server.py
@@ -31,6 +31,8 @@ with open('imagenet1000_clsidx_to_labels.txt', 'r', encoding='utf-8') as f:
 
 def preprocess_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    print(f"Original image size: ({image.size[0]}, {image.size[1]}) pixels")
+    image = image.resize((224, 224), Image.Resampling.BILINEAR)
     return image
 
 @app.get("/")
@@ -43,20 +45,19 @@ async def read_root():
 async def classify_image(file: UploadFile = File(...)):
     try:
         
-        # 파일이 이미지인지 확인
         if not file.content_type.startswith('image/'):
             raise HTTPException(status_code=400, detail="File must be an image")
         
-        print("파일 정보")
+        print("File info")
         print(f"Received file: {file.filename}")
         print(f"Content type: {file.content_type}")
         
         image_bytes = await file.read()
         
+        print("Start preprocessing image")
         image = preprocess_image(image_bytes)
         
-        # YOLO 모델 예측
-        print("모델 예측 시작")
+        print("Start model prediction")
         results = model(image)
         
         result = results[0].probs
@@ -73,15 +74,15 @@ async def classify_image(file: UploadFile = File(...)):
         return JSONResponse(content={"pred_label": top1, "pred_conf": top1conf, "pred_class": class_dict[top1]})
         
     except Exception as e:
-        print(f"에러 발생: {str(e)}")
-        print(f"에러 타입: {type(e)}")
+        print(f"Error: {str(e)}")
+        print(f"Error type: {type(e)}")
         raise HTTPException(status_code=422, detail=str(e))
 
 
 if __name__ == "__main__":
     import uvicorn
     import logging
-    # 로깅 설정
+    
     logging.basicConfig(level=logging.DEBUG)
     
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
